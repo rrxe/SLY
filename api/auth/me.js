4c3c7a0944b35253a6b86d1e0dc527b7b3c33c15
rrxe <rayanrayansra@gmail.com>
2026-08-12 21:50:44 +0300

    update

--- api/auth/me.js
+++ api/auth/me.js
@@ -3,7 +3,7 @@ import { authenticateRequest } from '../../lib/telegram-auth.js'
 
 const ENERGY_MAX = 5
 const ENERGY_REGEN_MS = 30 * 60 * 1000
-const REFERRAL_REWARD = 500
+const REFERRAL_REWARD = 1000
 
 function isSameUtcDay(dateA, dateB) {
   return (
