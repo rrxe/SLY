29f6c6cca0980ee828babeb811452fbd98db1c41
rrxe <rayanrayansra@gmail.com>
2026-08-17 01:07:56 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -43,7 +43,7 @@ export const ENERGY_MAX = 5;
 
 const GIGAPUB_PROJECT_ID = "7665";
 const GIGAPUB_SCRIPT_SRC = `https://ad.gigapub.tech/script?id=${GIGAPUB_PROJECT_ID}`;
-const REPEAT_AD_DELAY_MS = 90000;
+const REPEAT_AD_DELAY_MS = 40000;
 
 function makeId() {
   return `${Date.now()}-${Math.random().toString(16).slice(2)}`;
