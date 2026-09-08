c5f09395282dcaaee544cd4b082989c9097c7302
rrxe <rayanrayansra@gmail.com>
2026-09-08 15:03:13 +0300

    update

--- src/pages/Tasks.tsx
+++ src/pages/Tasks.tsx
@@ -546,7 +546,7 @@ export default function Tasks({ onRewardCoins }: Props) {
               data-debug="false"
               className="native-task-widget"
             >
-              <span slot="reward" className="task-reward native-task-reward">+550</span>
+              <span slot="reward" className="task-reward native-task-reward">+100</span>
               <div
                 slot="button"
                 className="task-btn join"
