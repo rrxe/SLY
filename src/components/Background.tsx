80f749c48a38fffd4be4aef5e20d9a5db2c74b01
rrxe <rayanrayansra@gmail.com>
2026-09-19 14:37:52 +0300

    update

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,5 +1,10 @@
 import "../styles/background.css";
 
 export default function Background() {
-  return <div className="app-bg" aria-hidden="true" />;
+  return (
+    <div className="app-bg" aria-hidden="true">
+      <div className="app-bg__stars app-bg__stars--a" />
+      <div className="app-bg__stars app-bg__stars--b" />
+    </div>
+  );
 }
